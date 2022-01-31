9fb00b3b4716c64b63a8c9fe5ed6bb03cf8b8f8e
RafaelRigunato <96690017+RafaelRigunato@users.noreply.github.com>
2022-01-30 23:23:24 -0300

    voto.c
    Elabore um programa em linguagem C, que receba a idade de uma pessoa e informe se ela é obrigada a votar nas eleições. Regra: pela idade, são obrigados a votar maiores de 18 e menores de 70.
    
    Saída esperada do seu programa: número 1 caso seja obrigatório, número 0 caso não seja obrigatório.

---
+++ voto.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int idade = 0;
+    
+    scanf("%d", &idade);
+    
+    if (idade >= 18 && idade <= 70) {
+        printf("%d", 1);
+    } else {
+        printf("%d", 0);
+    }
+        
+    
+    return 0;
+}
