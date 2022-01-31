717ca85eaaf94680e537afb22c62fc034bb0d01b
RafaelRigunato <96690017+RafaelRigunato@users.noreply.github.com>
2022-01-30 23:30:18 -0300

    DDD.c
    Elabore um programa que receba um valor inteiro representando um código de DDD para discagem interurbana. Em seguida, verifique e informe à qual cidade o DDD pertence, considerando a tabela abaixo:
    
    DDD	Cidade
    61	Brasilia
    71	Salvador
    11
    Sao Paulo
     21	 Rio de Janeiro
     19	Campinas 
     31	Belo Horizonte 
     62	Goiania

---
+++ DDD.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    
+    int ddd = 0;
+    
+    scanf("%d", &ddd);
+    if (ddd == 61) {
+        printf("Brasilia");
+        } else if (ddd == 71) {
+            printf("Salvador");
+            } else if (ddd == 11) {
+                printf("Sao Paulo");
+                } else if (ddd == 21) {
+                    printf("Rio de Janeiro");
+                    } else if (ddd == 19) {
+                        printf("Campinas");
+                        } else if (ddd == 31) {
+                            printf("Belo Horizonte");
+                            } else if (ddd == 62) {
+                                printf("Goiania");
+                            }
+    
+    return 0;
+}
